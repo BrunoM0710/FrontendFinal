6b6821a1898a6f891efb8179a4f79bc1daa9b38a
BrunoM0701 <bm329978@fi365.ort.edu.uy>
2026-06-11 14:13:55 -0300

    Update PerfumeCard.jsx

--- src/features/perfumes/components/PerfumeCard.jsx
+++ src/features/perfumes/components/PerfumeCard.jsx
@@ -1,7 +1,7 @@
 import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
 
-import "./perfumeCard.css";
+import "./perfumeCard.css"; 
 
 const PerfumeCard = ({ perfume }) => {
   const navigate = useNavigate();
